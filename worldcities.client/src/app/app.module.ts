9e454388fbc8eb025ae9b4d91934c95052d90ccd
Geert Dierick <geert2dierick@gmail.com>
2026-01-22 13:28:24 +0100

    Add material table and paging to Cities component

--- worldcities.client/src/app/app.module.ts
+++ worldcities.client/src/app/app.module.ts
@@ -7,24 +7,22 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { CitiesComponent } from './cities/cities.component';
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    CitiesComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatToolbarModule,
+    AngularMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
